b4e8e327cb2d099002b0f0d00bdc128a20f36178
kenlasko <ken.lasko@gmail.com>
2024-11-21 15:51:16 -0500

    Sync script update

--- mariadb-standalone/README.md
+++ mariadb-standalone/README.md
@@ -2,6 +2,10 @@
 This serves as a realtime backup for the [MariaDB Galera cluster](/mariadb-galera) running in the same cluster. It's intended for a situation where the Galera cluster is completely destroyed (usually because of something I've done). Since I've gotten a hang of running the cluster, this instance has seen very little use and may eventually be decommissioned, although I actually used it for a restore source just the other week (2024-Nov).
 
 # Replication Setup
+The [database-sync.sh](/mariadb/scripts/database-sync.sh) script automates the backup, restore and sync config for all MariaDB deployments. If it does not work, the manual steps are in the following sections. Simply run:
+```
+./k3s/mariadb/scripts/database-sync.sh
+```
 ## Primary DB Backup
 Run `mariadb-backup-sync` job from `mariadb` namespace on Home cluster. Do via either ArgoCD or:
 ```
--- mariadb/scripts/database-sync.sh
+++ mariadb/scripts/database-sync.sh
@@ -7,28 +7,30 @@ BACKUP_NAMESPACE="mariadb-test"
 BACKUP_JOB_NAME="mariadb-backup-sync"
 RESTORE_JOB_NAME="mariadb-restore-sync"
 
-# Check if the backup job exists
-echo "Checking if job $BACKUP_JOB_NAME exists in namespace $BACKUP_NAMESPACE..."
+JOB_DATA=(
+  "home,mariadb-standalone,mariadb-restore"
+  "home,mariadb-test,mariadb-restore-nas01"
+  "cloud,mariadb,mariadb-restore"
+)
+
+# Check if an old backup job exists, and delete it if it does.
 if kubectl get job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" > /dev/null 2>&1; then
   echo "Job $BACKUP_JOB_NAME exists. Deleting it..."
-  kubectl delete job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME"
-else
-  echo "Job $BACKUP_JOB_NAME does not exist in namespace $BACKUP_NAMESPACE."
+  kubectl delete job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" --dry-run=server
 fi
 
 # Create the backup job
-echo "Creating backup job in namespace ${BACKUP_NAMESPACE}..."
-kubectl create job -n "$BACKUP_NAMESPACE" --from=cronjob/mariadb-backup-sync "$BACKUP_JOB_NAME"
+kubectl create job -n "$BACKUP_NAMESPACE" --from=cronjob/mariadb-backup-sync "$BACKUP_JOB_NAME" --dry-run=server
 
 # Wait for the backup job to complete
 echo "Waiting for backup job to complete..."
 while true; do
-  JOB_STATUS=$(kubectl get job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" -o jsonpath='{.status.succeeded}')
+  JOB_STATUS=$(kubectl get job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" -o jsonpath='{.status.succeeded}' --dry-run=client)
   if [ "$JOB_STATUS" == "1" ]; then
     echo "Backup job completed successfully."
     break
   fi
-  FAILED_STATUS=$(kubectl get job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" -o jsonpath='{.status.terminating}')
+  FAILED_STATUS=$(kubectl get job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" -o jsonpath='{.status.terminating}' --dry-run=client)
   if [ "$FAILED_STATUS" -ge 1 ]; then
     echo "Backup job failed."
     exit 1
@@ -36,12 +38,17 @@ while true; do
   sleep 5
 done
 
-# Create the restore jobs
-echo "Creating restore job in mariadb-standalone namespace..."
-kubectl create job --context home -n mariadb-standalone --from=cronjob/mariadb-restore "$RESTORE_JOB_NAME"
-echo "Creating restore job on NAS01..."
-kubectl create job --context home -n "$BACKUP_NAMESPACE" --from=cronjob/mariadb-restore-nas01 "$RESTORE_JOB_NAME"
-echo "Creating restore job in mariadb namespace in cloud context..."
-kubectl create job --context cloud -n mariadb --from=cronjob/mariadb-restore "$RESTORE_JOB_NAME"
+
+for DATA in "${JOB_DATA[@]}"; do
+  # Split the string into variables
+  IFS=',' read -r CONTEXT NAMESPACE CRONJOB <<< "$DATA"
+
+  # Check if the restore job exists
+  if kubectl get job --context "$CONTEXT" -n "$NAMESPACE" "$RESTORE_JOB_NAME" > /dev/null 2>&1; then
+    kubectl delete job --context "$CONTEXT" -n "$NAMESPACE" "$RESTORE_JOB_NAME" --dry-run=server
+  fi
+
+  kubectl create job --context "$CONTEXT" -n "$NAMESPACE" --from="cronjob/$CRONJOB" "$RESTORE_JOB_NAME" --dry-run=server
+done
 
 echo "All restore jobs triggered successfully."
