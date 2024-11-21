111aebba3f49c95f602bfee51578f4319c88723f
kenlasko <ken.lasko@gmail.com>
2024-11-21 11:24:29 -0500

    Database sync script update

--- mariadb/scripts/database-sync.sh
+++ mariadb/scripts/database-sync.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+# This script will setup replication between the primary Galera cluster and the mariadb-standalone and cloud mariadb instances
+# This script is to be run after the primary database cluster is up and running. 
 
 # Set namespace and job names
 BACKUP_NAMESPACE="mariadb-test"
 BACKUP_JOB_NAME="mariadb-backup-sync"
 RESTORE_JOB_NAME="mariadb-restore-sync"
+
+# Check if the backup job exists
+echo "Checking if job $BACKUP_JOB_NAME exists in namespace $BACKUP_NAMESPACE..."
+if kubectl get job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" > /dev/null 2>&1; then
+  echo "Job $BACKUP_JOB_NAME exists. Deleting it..."
+  kubectl delete job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME"
+else
+  echo "Job $BACKUP_JOB_NAME does not exist in namespace $BACKUP_NAMESPACE."
+fi
 
 # Create the backup job
 echo "Creating backup job in namespace ${BACKUP_NAMESPACE}..."
@@ -17,7 +28,7 @@ while true; do
     echo "Backup job completed successfully."
     break
   fi
-  FAILED_STATUS=$(kubectl get job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" -o jsonpath='{.status.failed}')
+  FAILED_STATUS=$(kubectl get job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" -o jsonpath='{.status.terminating}')
   if [ "$FAILED_STATUS" -ge 1 ]; then
     echo "Backup job failed."
     exit 1
@@ -25,7 +36,7 @@ while true; do
   sleep 5
 done
 
-# Create the restore jobs in mariadb-standalone and cloud instances
+# Create the restore jobs
 echo "Creating restore job in mariadb-standalone namespace..."
 kubectl create job --context home -n mariadb-standalone --from=cronjob/mariadb-restore "$RESTORE_JOB_NAME"
 echo "Creating restore job in mariadb namespace in cloud context..."
