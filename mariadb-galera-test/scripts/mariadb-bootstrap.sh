0fd47ee99d9dab4503e6c00ec1e75a03756dd916
kenlasko <ken.lasko@gmail.com>
2024-11-22 11:18:56 -0500

    Fix restore script

--- mariadb-galera-test/scripts/mariadb-bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This script will set grastate.dat safetoboostrap to 1 for enabling a Galera cluster
-
-FILE_PATH="/host/var/mariadb-galera/storage/grastate.dat"
-
-kubectl debug node/$1 -it --image='nicolaka/netshoot' -n kube-system -- /bin/sh -c "sed -i -e 's/safe_to_bootstrap: 0/safe_to_bootstrap: 1/' $FILE_PATH; exit"
-# Delete debugger pod after completion
-kubectl get pods -n kube-system --no-headers=true  | awk '/node-debugger/{print $1}' | xargs kubectl delete -n kube-system pod
--- mariadb/scripts/database-sync.sh
+++ mariadb/scripts/database-sync.sh
@@ -16,21 +16,21 @@ JOB_DATA=(
 # Check if an old backup job exists, and delete it if it does.
 if kubectl get job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" > /dev/null 2>&1; then
   echo "Job $BACKUP_JOB_NAME exists. Deleting it..."
-  kubectl delete job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" --dry-run=server
+  kubectl delete job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME"
 fi
 
 # Create the backup job
-kubectl create job -n "$BACKUP_NAMESPACE" --from=cronjob/mariadb-backup-sync "$BACKUP_JOB_NAME" --dry-run=server
+kubectl create job -n "$BACKUP_NAMESPACE" --from=cronjob/mariadb-backup-sync "$BACKUP_JOB_NAME"
 
 # Wait for the backup job to complete
 echo "Waiting for backup job to complete..."
 while true; do
-  JOB_STATUS=$(kubectl get job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" -o jsonpath='{.status.succeeded}' --dry-run=client)
+  JOB_STATUS=$(kubectl get job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" -o jsonpath='{.status.succeeded}')
   if [ "$JOB_STATUS" == "1" ]; then
     echo "Backup job completed successfully."
     break
   fi
-  FAILED_STATUS=$(kubectl get job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" -o jsonpath='{.status.terminating}' --dry-run=client)
+  FAILED_STATUS=$(kubectl get job -n "$BACKUP_NAMESPACE" "$BACKUP_JOB_NAME" -o jsonpath='{.status.terminating}')
   if [ "$FAILED_STATUS" -ge 1 ]; then
     echo "Backup job failed."
     exit 1
@@ -45,10 +45,10 @@ for DATA in "${JOB_DATA[@]}"; do
 
   # Check if the restore job exists
   if kubectl get job --context "$CONTEXT" -n "$NAMESPACE" "$RESTORE_JOB_NAME" > /dev/null 2>&1; then
-    kubectl delete job --context "$CONTEXT" -n "$NAMESPACE" "$RESTORE_JOB_NAME" --dry-run=server
+    kubectl delete job --context "$CONTEXT" -n "$NAMESPACE" "$RESTORE_JOB_NAME"
   fi
-
-  kubectl create job --context "$CONTEXT" -n "$NAMESPACE" --from="cronjob/$CRONJOB" "$RESTORE_JOB_NAME" --dry-run=server
+  echo "Creating backup job $CONTEXT/$CRONJOB"
+  kubectl create job --context "$CONTEXT" -n "$NAMESPACE" --from="cronjob/$CRONJOB" "$RESTORE_JOB_NAME"
 done
 
 echo "All restore jobs triggered successfully."
