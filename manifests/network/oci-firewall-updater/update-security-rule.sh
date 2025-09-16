3459a275e9f35b0f022621e40ae1b4d61c8ec789
Ken Lasko <ken.lasko@gmail.com>
2025-09-15 21:10:02 -0400

    Add script

---
+++ manifests/network/oci-firewall-updater/update-security-rule.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DNS_NAME="home.ucdialplans.com"
+SECURITY_LIST_ID="ocid1.securitylist.oc1.ca-toronto-1.aaaaaaaajhnvoq3w4nsfb2pigc2icp4vczxcufq7v3b42jjunubdc6oma7sa"
+
+# Get the current public IP address of the DNS name
+HOME_IP=$(ping -c1 -w1 $DNS_NAME | awk -F'[()]' '/PING/{print $2}')
+
+# Get the current security rules
+INGRESS_RULES=$(oci network security-list get --security-list-id "$SECURITY_LIST_ID" | jq '.data."ingress-security-rules"')
+
+# Extract the current home IP from the ingress rules
+CURRENT_IP=$(echo "$INGRESS_RULES" \
+  | jq -r '.[] | select(.description=="Allow all for home access") | .source' \
+  | sed 's#/32##')
+
+if [ "$HOME_IP" != "$CURRENT_IP" ]; then
+    echo "Updating security list. Old IP: $CURRENT_IP, New IP: $HOME_IP"
+    # Update the security list with the new IP
+    UPDATED_RULES=$(echo "$INGRESS_RULES" | sed "s#${CURRENT_IP}#${HOME_IP}#")
+    # Apply the updated rules
+    oci network security-list update \
+    --security-list-id "$SECURITY_LIST_ID" \
+    --ingress-security-rules "$UPDATED_RULES"
+else
+    echo "No update needed. Current IP: $CURRENT_IP"
+fi
